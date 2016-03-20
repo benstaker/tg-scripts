2045b5dbc15caa6b7a19bd28204f1cf8fc3a7f96
Ben Staker <ben@benstaker.com>
2016-03-20 12:34:22 +0000

    Typo

--- core/core.lua
+++ core/core.lua
@@ -2,7 +2,7 @@
 
 -- Include the core modules
 package.path = "../?.lua;" .. package.path
-local functions = require(moduleFolder .. 'core.functions')
+local functions = require(moduleFolder .. '.core.functions')
 
 -- Attach the core modules
 local module = {}
